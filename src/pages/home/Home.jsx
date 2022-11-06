b944ede83ed240342f7cc4c330554a841383f844
ibro001 <ibrahimmoses56@gmail.com>
2022-11-06 18:30:07 +0100

    rendered the featuredProperties component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
@@ -15,6 +16,10 @@ const Home = () => {
           Browse by property type
         </h1>
         <PropertyList />
+        <h1 className="homeTitle">
+          Home guests love
+        </h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
