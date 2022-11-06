6a014ffeba95eff1bf5f0c5eaac603d3039edce5
ibro001 <ibrahimmoses56@gmail.com>
2022-11-06 07:07:53 +0100

    init the app componenet and setup the route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
+import List from './pages/list/List.jsx'
+import Hotel from './pages/hotel/Hotel.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List />} />
+        <Route path='/hotels/:id' element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
